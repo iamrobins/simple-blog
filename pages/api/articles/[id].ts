1fcc5ad170c281757b026194e294a560f3d9aea3
robin <itsrobin4@gmail.com>
2021-02-25 22:51:38 +0530

    simple blog with nextjs init

---
+++ pages/api/articles/[id].ts
@@ -0,0 +1,12 @@
+import {articles} from "../../../data";
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  const {id} = req.query;
+  const filtered = articles.filter(article => article.id === id);
+  if(filtered.length > 0) {
+    return res.status(200).json(filtered);
+  } else {
+    return res.status(404).json({Message: `Article with id ${id} is not found`});
+  }
+}
---
+++ pages/api/articles/index.ts
@@ -0,0 +1,6 @@
+import {articles} from "../../../data";
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json(articles);
+}
